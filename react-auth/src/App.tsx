2f2b72a1f41127456726f0544d46f49a291e9b76
galifornia <martinho87@gmail.com>
2021-10-27 17:59:56 +0200

    [Frontend] Add bootstrap & login template

--- react-auth/src/App.tsx
+++ react-auth/src/App.tsx
@@ -4,21 +4,40 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <main className='form-signin'>
+        <form>
+          <h1 className='h3 mb-3 fw-normal'>Please sign in</h1>
+
+          <div className='form-floating'>
+            <input
+              type='email'
+              className='form-control'
+              id='floatingInput'
+              placeholder='name@example.com'
+            />
+            <label htmlFor='floatingInput'>Email address</label>
+          </div>
+          <div className='form-floating'>
+            <input
+              type='password'
+              className='form-control'
+              id='floatingPassword'
+              placeholder='Password'
+            />
+            <label htmlFor='floatingPassword'>Password</label>
+          </div>
+
+          <div className='checkbox mb-3'>
+            <label htmlFor='remember-me'>Remember me</label>
+            <input id='remember-me' type='checkbox' value='remember-me' />
+          </div>
+          <button className='w-100 btn btn-lg btn-primary' type='submit'>
+            Sign in
+          </button>
+          <p className='mt-5 mb-3 text-muted'>&copy; 2017–2021</p>
+        </form>
+      </main>
     </div>
   );
 }
