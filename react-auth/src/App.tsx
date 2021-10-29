73afbca97adbd07c615fee428f09aa2075753f2a
galifornia <martinho87@gmail.com>
2021-10-29 09:46:08 +0200

    [Frontend]: add Navigation bar

--- react-auth/src/App.tsx
+++ react-auth/src/App.tsx
@@ -5,6 +5,29 @@ import './App.css';
 function App() {
   return (
     <div className='App'>
+      <nav className='navbar navbar-expand-md navbar-dark bg-dark mb-4'>
+        <div className='container-fluid'>
+          <div>
+            <ul className='navbar-nav me-auto mb-2 mb-md-0'>
+              <li className='nav-item'>
+                <a className='nav-link active' aria-current='page' href='#'>
+                  Home
+                </a>
+              </li>
+              <li className='nav-item'>
+                <a className='nav-link' href='#'>
+                  LogIn
+                </a>
+              </li>
+              <li className='nav-item'>
+                <a className='nav-link' href='#'>
+                  Register
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
       <main className='form-signin'>
         <form>
           <h1 className='h3 mb-3 fw-normal'>Please sign in</h1>
@@ -28,14 +51,9 @@ function App() {
             <label htmlFor='floatingPassword'>Password</label>
           </div>
 
-          <div className='checkbox mb-3'>
-            <label htmlFor='remember-me'>Remember me</label>
-            <input id='remember-me' type='checkbox' value='remember-me' />
-          </div>
           <button className='w-100 btn btn-lg btn-primary' type='submit'>
             Sign in
           </button>
-          <p className='mt-5 mb-3 text-muted'>&copy; 2017–2021</p>
         </form>
       </main>
     </div>
