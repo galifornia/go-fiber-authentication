247a3e453e0b2eeb3d8903b38017618ae98f717d
galifornia <martinho87@gmail.com>
2021-10-31 16:13:40 +0100

    [Frontend] Add routes using react-router-dom

--- react-auth/src/App.tsx
+++ react-auth/src/App.tsx
@@ -1,14 +1,21 @@
 import './App.css';
 import Login from './pages/Login';
 import Navigation from './components/Navigation';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { Home } from './pages/Home';
+import Register from './pages/Register';
 
 function App() {
   return (
     <div className='App'>
-      <Navigation />
-      <main className='form-signin'>
-        <Login />
-      </main>
+      <BrowserRouter>
+        <Navigation />
+        <main className='form-signin'>
+          <Route path='/' exact component={Home}></Route>
+          <Route path='/login' component={Login}></Route>
+          <Route path='/register' component={Register}></Route>
+        </main>
+      </BrowserRouter>
     </div>
   );
 }
--- react-auth/src/components/Navigation.tsx
+++ react-auth/src/components/Navigation.tsx
@@ -1,3 +1,6 @@
+import { Link } from 'react-router-dom';
+import { Home } from '../pages/Home';
+
 const Navigation = () => {
   return (
     <nav className='navbar navbar-expand-md navbar-dark bg-dark mb-4'>
@@ -5,19 +8,19 @@ const Navigation = () => {
         <div>
           <ul className='navbar-nav me-auto mb-2 mb-md-0'>
             <li className='nav-item'>
-              <a className='nav-link active' aria-current='page' href='#'>
+              <Link className='nav-link active' aria-current='page' to='/'>
                 Home
-              </a>
+              </Link>
             </li>
             <li className='nav-item'>
-              <a className='nav-link' href='#'>
+              <Link className='nav-link' to='/login'>
                 LogIn
-              </a>
+              </Link>
             </li>
             <li className='nav-item'>
-              <a className='nav-link' href='/register'>
+              <Link className='nav-link' to='/register'>
                 Register
-              </a>
+              </Link>
             </li>
           </ul>
         </div>
